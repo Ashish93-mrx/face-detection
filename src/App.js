0dae171c913776cf1c0bc5cc089a92f7f04f0eda
Ashish <ashishcoorg46@gmail.com>
2023-08-22 22:14:20 +0530

    new backend hostlink added

--- src/App.js
+++ src/App.js
@@ -153,7 +153,8 @@ onButtonSubmit = () => {
         //.then(response => console.log(response))
         .then(response => {
           this.displayFaceBox(this.calculateFaceLocation(response))
-          
+        })
+        .catch(err => console.log(err));
         if(response){          
           fetch('https://shiv-vfze.onrender.com/image',{
             method:'put',
@@ -168,9 +169,9 @@ onButtonSubmit = () => {
             })
             .catch(err => console.log(err));
           }
-        })
+        
       //.then(response => response.json())
-      .catch(err => console.log(err));
+      
 }    
 
 
